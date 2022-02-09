f35178d9614afe8328805f4d345788b3d3729bb6
Sami Ghayeni <asedsami@gmail.com>
2022-02-09 13:58:14 +0330

    Migrate index.js to index.ts

---
+++ index.ts
@@ -0,0 +1,24 @@
+type ObjectWithPath = {
+  [key: string]: any
+}
+
+//...rest of the code
+
+export default function dotpath(str: string) {
+  const parts = str.split('.')
+  const len = parts.length
+
+  return function parse(obj: ObjectWithPath) {
+    var testKey
+
+    for (var i = 0; i < len; ++i) {
+      testKey = parts[i]
+
+      if (!obj) return
+
+      obj = obj[testKey]
+    }
+
+    return obj
+  }
+}
